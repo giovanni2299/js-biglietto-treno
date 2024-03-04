5c6f5fc66b0edf31030d4d148eeb30b74b66d686
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-03-04 13:46:52 +0100

    ADD:first element of the document and few rules

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// console.log('ciao')
+
+
+//DATI DEL PROBLEMA
+//età passeggero fornità dal passeggero
+//costo 0,21$ AL KM
+//Sconto minorenni -20%
+//over 65 -40% di sconto 
+//calcolare il prezzo del biglietto in base ai km e al età del passeggero
+
+//AGE
+let age = parseFloat(prompt('inserisci la tua età')); //number
+console.log('età:',age); //number
+
+//KM
+let KM = parseFloat(prompt('write your hage'))
