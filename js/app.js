d789fe5da8bfe8b9b07fcc5bd9795bd1c6c50220
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-03-04 17:57:01 +0100

    Refactin:code texture

--- js/app.js
+++ js/app.js
@@ -19,20 +19,32 @@ console.log('distance to do in KM:',KM); //number
 //TIKET-PRICE
 let price = parseFloat(KM * 0.21); //number
 
+
+
 //discount 40%
-let priceOff40 = parseInt(price * 0.4); //number
-let discountPrice40 = price - priceOff40
+// let priceOff40 = parseInt(price * 0.4); //number
+// let discountPrice40 = price - priceOff40
 
 //discount 20%
-let priceOff20 = parseInt(price * 0.2); //number
-let discountPrice20 = price - priceOff20
+// let priceOff20 = parseInt(price * 0.2); //number
+// let discountPrice20 = price - priceOff20
+
+
 
 //SE over 65 applico uno sconto del 40%
 if(age >=65){
+    //discount 40%
+    let priceOff40 = parseInt(price * 0.4); //number
+    let discountPrice40 = price - priceOff40
     console.log('tiketoff40:' ,discountPrice40.toFixed(2)); //string
+
     //Altrimenti se minorenne applico uno sconto del 20%
 }else if(age <18){
+    //discount 20%
+    let priceOff20 = parseInt(price * 0.2); //number
+    let discountPrice20 = price - priceOff20
     console.log('ticketoff20:' ,discountPrice20.toFixed(2)); //string
+
     //Altrimenti niente sconto 
 }else{
     console.log('full price:' ,price.toFixed(2)); //string
