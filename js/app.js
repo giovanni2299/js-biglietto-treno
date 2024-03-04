a5cdba3a557621710c80066a6b6394755f6ae3f9
giovanni2299 <giovanni-sorrentino@hotmail.it>
2024-03-04 14:21:12 +0100

    ADD:quest for solve the document and solution

--- js/app.js
+++ js/app.js
@@ -9,8 +9,23 @@
 //calcolare il prezzo del biglietto in base ai km e al età del passeggero
 
 //AGE
-let age = parseFloat(prompt('inserisci la tua età')); //number
+let age = parseFloat(prompt('write your age')); //number
 console.log('età:',age); //number
 
 //KM
-let KM = parseFloat(prompt('write your hage'))
+let KM = parseFloat(prompt('write your distance')); //number
+console.log('distance:',KM); //number
+
+//TIKET-PRICE
+let price = parseFloat(KM * 0,21); //number
+if(age >=18 || age >=65){
+    console.log('full price:',price);
+}
+
+//discounnt 20%
+let price20 = parseFloat(price * 0.2); //number 
+if(age <18){
+    console.log('ticket20:' ,price - price20);
+}
+
+//discount 40%
